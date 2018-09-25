7332deb545acf2e20d2f8e697660b9f4201a850e
Sydney Grant <sg3957@nyu.edu>
2018-09-25 02:04:14 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,42 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase.upcase != phrase
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
+  if phrase "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif
+  if phrase.upcase != phrase
+    return "HUH?! "
   else
     return "NO, NOT SINCE 1938!"
 end
+
+
+
+  it 'responds with HUH?! SPEAK UP, SONNY! unless you are shouting' do
+    expect(speak_to_grandma('Hi Nana, how are you?')).to eq 'HUH?! SPEAK UP, SONNY!'
+  end
+
+  it 'responds with HUH?! SPEAK UP, SONNY! for a second time' do
+    expect(speak_to_grandma('Hi!')).to eq 'HUH?! SPEAK UP, SONNY!'
+  end
+
+  it 'responds with NO, NOT SINCE 1938! when she can hear you' do
+    expect(speak_to_grandma('WHAT DID YOU EAT TODAY?')).to eq "NO, NOT SINCE 1938!"
+  end
+
+  it 'responds with NO, NOT SINCE 1938! for a second time' do
+    expect(speak_to_grandma('WHAT?')).to eq "NO, NOT SINCE 1938!"
+  end
+
+  it 'responds with I LOVE YOU TOO PUMPKIN! if you say I LOVE YOU GRANDMA!' do
+    expect(speak_to_grandma('I LOVE YOU GRANDMA!')).to eq 'I LOVE YOU TOO PUMPKIN!'
+  end
+ 
+  it 'responds with HUH?! SPEAK UP, SONNY! unless you are shouting' do
+    expect(speak_to_grandma('Hi Nana, how are you?')).to eq 'HUH?! SPEAK UP, SONNY!'
+  end
+
+end
+
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
