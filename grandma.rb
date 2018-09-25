553011db2c165a4e6c0919f35200075858ba8b53
Sydney Grant <sg3957@nyu.edu>
2018-09-25 02:00:59 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,8 @@
 def speak_to_grandma(phrase)
   if phrase.upcase != phrase
     return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.upcase == phrase
     return "NO, NOT SINCE 1938!"
 end
